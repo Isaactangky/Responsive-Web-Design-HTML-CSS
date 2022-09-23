e6608f7a84e6a099e281239cccb16eb68d4da1ac
Isaactangky <75126969+Isaactangky@users.noreply.github.com>
2022-09-23 11:28:36 +0800

    complete design of intro page challenge
    a challenge from Frontend Mentro

---
+++ frontendmentor-intro-section-with-dropdown-navigation-main/index.js
@@ -0,0 +1,39 @@
+const openMenu = document.querySelector(".menu");
+const closeMenu = document.querySelector(".close-menu");
+
+const overlay = document.querySelector(".overlay");
+const mainMenu = document.querySelector(".navbar-user-wrapper");
+const mainMenuLiEls = document.querySelectorAll(".main-menu >li");
+const subMenuEls = document.querySelectorAll(".sub-menu");
+function hideElements() {
+  overlay.classList.toggle("hidden");
+  mainMenu.classList.toggle("active");
+}
+openMenu.addEventListener("click", function () {
+  overlay.classList.toggle("hidden");
+  mainMenu.classList.toggle("active");
+});
+
+// Hide overlay and main menu
+overlay.addEventListener("click", hideElements);
+closeMenu.addEventListener("click", hideElements);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !overlay.classList.contains("hidden"))
+    hideElements();
+});
+// sub-menu
+mainMenuLiEls.forEach((li) =>
+  li.addEventListener("click", function () {
+    const subMenu = this.querySelector(".sub-menu");
+    if (!subMenu) return;
+    subMenu.classList.toggle("hidden");
+  })
+);
+document.addEventListener("click", function (e) {
+  if (
+    !e.target.classList.contains("features") &&
+    !e.target.classList.contains("company")
+  ) {
+    subMenuEls.forEach((el) => el.classList.add("hidden"));
+  }
+});
